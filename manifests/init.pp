0e0ac210bb954b501468aba07e2d5fb26f96221a
Reid Vandewiele <reid@puppetlabs.com>
2018-06-06 17:18:40 -0700

    Started working on math

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,19 +8,26 @@
 #   include puppet_agent_scheduler
 class puppet_agent_scheduler (
   Integer $run_interval = 30,
-  String  $start_time   = '00:00',
+  String  $start_time   = '16:00',
   Boolean $splay        = true,
   Integer $splay_limit  = $run_interval,
 ) {
-
   $run_interval_minutes = $run_interval
-  $splay_limit_minutes = $splay_limit
-  $splay_minutes = fqdn_rand($splay_limit_minutes, 'puppet_agent_scheduler')
 
   # Validation
   if ((1440 % $run_interval_minutes) != 0) {
     fail("puppet_agent_scheduler::run_interval must be a multiple of 24h")
   }
+
+  $splay_limit_minutes = $splay_limit
+  $splay_minutes = fqdn_rand($splay_limit_minutes, 'puppet_agent_scheduler')
+  $start_hour   = Integer($start_time[0,2])
+  $start_min    = Integer($start_time[3,2])
+
+  $adj_start_mins_since_midnight = $start_hour * 60 + $start_min + $splay_minutes
+
+  $adj_start_hour = $adj_start_mins_since_midnight / 60
+  $adj_start_min  = $adj_start_mins_since_midnight % 60
 
   case $facts[os][family] {
     'windows': { contain puppet_agent_scheduler::windows }
--- manifests/posix.pp
+++ manifests/posix.pp
@@ -7,4 +7,15 @@
 # @example
 #   include puppet_agent_scheduler::posix
 class puppet_agent_scheduler::posix {
+  assert_private()
+
+  #$puppet_agent_scheduler::run_interval_minutes
+  #$puppet_agent_scheduler::adj_start_mins_since_midnight
+  $start_hour = $puppet_agent_scheduler::adj_start_hour
+  $start_min  = $puppet_agent_scheduler::adj_start_min
+
+  notify { 'start min': message => $start_min; }
+  notify { 'start hour': message => $start_hour; }
+
+
 }
