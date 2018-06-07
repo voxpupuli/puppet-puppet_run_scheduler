8d53d7c0d673f500f743a218b5f338feff7f3860
Reid Vandewiele <reid@puppetlabs.com>
2018-06-07 16:51:05 -0700

    Remove splay param, change splaylimit
    Make the parameter name the same as the puppet config setting name,
    including no "_"

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,13 +10,12 @@ class puppet_run_scheduler (
   Enum['present', 'absent']          $ensure       = 'present',
   Puppet_run_scheduler::Run_interval $run_interval = '30m',
   Pattern[/[0-2]\d:\d\d/]            $start_time   = '00:00',
-  Boolean                            $splay        = true,
-  Puppet_run_scheduler::Run_interval $splay_limit  = '30m',
+  Puppet_run_scheduler::Run_interval $splaylimit   = '30m',
 ) {
   $interval_mins = puppet_run_scheduler::minutes($run_interval)
-  $splay_limit_mins = puppet_run_scheduler::minutes($splay_limit)
+  $splaylimit_mins = puppet_run_scheduler::minutes($splaylimit)
 
-  $splay_mins = fqdn_rand($splay_limit_mins, 'puppet_run_scheduler')
+  $splay_mins = fqdn_rand($splaylimit_mins, 'puppet_run_scheduler')
   $input_start_hour = Integer($start_time[0,2])
   $input_start_min  = Integer($start_time[3,2])
 
